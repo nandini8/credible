4e59fe73602d334127cd0a0a1c6a661513192c8b
nandini <nandini.soni8@gmail.com>
2018-03-30 19:21:42 +0530

    Allowed host changed to all

--- credible_project/settings.py
+++ credible_project/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'ml4q9(wafizw$^rrr9e*^xdd+8^m8bz5#$53vef2i)3w-xcd(v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-13-58-198-25.us-east-2.compute.amazonaws.com', 'ec2-18-220-1-211.us-east-2.compute.amazonaws.com', '18.220.1.211', '127.0.0.1']
-#ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['ec2-13-58-198-25.us-east-2.compute.amazonaws.com', 'ec2-18-220-1-211.us-east-2.compute.amazonaws.com', '18.220.1.211', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
