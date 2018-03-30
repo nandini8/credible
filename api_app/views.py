ec4778419ce5448956473d903d9910836a53f76f
nandini <nandini.soni8@gmail.com>
2018-03-31 01:12:32 +0530

    changed api fields

--- api_app/views.py
+++ api_app/views.py
@@ -24,7 +24,7 @@ def article(request):
 		db='CredibleDB')
 	cursor = mydb.cursor()
 	mydb.set_character_set('utf8mb4')
-	query = 'SELECT article_id, title, content, credibility_score FROM TWEET_DATA;'
+	query = 'SELECT article_id, title, content, credibility_score, publisher, published_date, article_link FROM TWEET_DATA;'
 	cursor.execute(query)
 	article_details = cursor.fetchall()
 	articles = []
@@ -34,6 +34,9 @@ def article(request):
 		d['title'] = row[1]
 		d['content'] = row[2]
 		d['credibility_score'] = str(row[3])
+		d['publisher'] = row[4]
+		d['published_date'] = row[5]
+		d['article_link'] = row[6]
 		articles.append(d)
 	j = json.dumps(articles, default = myconverter)
 	cursor.close()
@@ -49,7 +52,7 @@ def articles_detail(request, article_number):
 		db='CredibleDB')
 	cursor = mydb.cursor()
 	mydb.set_character_set('utf8mb4')
-	query = 'SELECT article_id, title, content, publisher, published_date, credibility_score FROM TWEET_DATA WHERE article_id = ' + article_number + ';'
+	query = 'SELECT article_id, title, content, publisher, published_date, credibility_score, article_link FROM TWEET_DATA WHERE article_id = ' + article_number + ';'
 	print(query)
 	print("Article number", article_number)
 	cursor.execute(query)
@@ -63,6 +66,7 @@ def articles_detail(request, article_number):
 		d['publisher'] = row[3]
 		d['published_date'] = row[4]
 		d['credibility_score'] = str(row[5])
+		d['article_link'] = row[6]
 		articles.append(d)
 	j = json.dumps(articles, default = myconverter)
 	cursor.close()
